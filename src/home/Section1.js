9aa8e3288d4b0af25b1ff309879cd036221de854
Om Trivedi <omtrivedioo3@gmail.com>
2024-11-28 00:32:39 +0530

    second commit

--- src/home/Section1.js
+++ src/home/Section1.js
@@ -5,7 +5,12 @@ import Heading1 from "./Heading1.png";
 import HomeEmg from "./HomeEmg.png";
 import Clip from "./Clip.png";
 import image1 from "./Image1.png";
-import image2 from "./Frame851.png";
+import Frame851 from "./Frame851.png";
+import image2 from "./Image2.png";
+import Frame840 from "./Frame840.png";
+import Background from "./Background.png";
+import Background1 from "./Background(1).png";
+import Container from "./Container.png";
 
 function Section1() {
   return (
@@ -24,22 +29,52 @@ function Section1() {
       <button className="bg-[#31BF5C] text-white font-Roboto py-2 px-2 mx-10 my-6 top-10 left-10 rounded-3xl">
         Free Download
       </button>
-      <div class="flex items-center justify-center h-50vh relative top-44">
-        <div class="flex items-center justify-center h-50vh absolute  left-1/3 -top-16">
-          <img src={Clip} className=" w-10px h-10 rounded-xl"></img>
+      <div>
+        <div className="flex items-center justify-center my-12 top-14 relative">
+          <div class="flex items-center justify-center  h-50vh absolute -top-11">
+            <div class="flex items-center justify-center h-50vh absolute  left-1/3 -top-16">
+              {/* green errows over center image */}
+              <img src={Clip} className=" w-10px h-10 rounded-xl"></img>
+            </div>
+            {/* black window over green image */}
+            <div class="flex items-center justify-center h-50vh absolute  left-50 -top-10 ">
+              <img src={HomeEmg} className=" w-1120px h-60 rounded-xl"></img>
+            </div>
+            {/* green image */}
+            <img src={bg1} className=" w-1120px h-60 rounded-xl"></img>
+          </div>
+          {/* actipace total security image */}
+          <div className="flex items-center  justify-center h-10 bottom-0 absolute">
+            <div className="flex items-center  justify-center h-10 bottom-1/3 absolute">
+              <img src={Frame851} alt="" className="w-1/2 h-100" />
+            </div>
+            {/* half green image */}
+            <img src={image2} alt="" className="w h-100 top-3" />
+          </div>
+          {/*  its to easy to install big bg image*/}
+          <img src={image1} alt="" className="w-100 h-1/4 top-14 left-16" />
         </div>
-        <div class="flex items-center justify-center h-50vh absolute  left-50 -top-10 ">
-          <img src={HomeEmg} className=" w-1120px h-60 rounded-xl"></img>
+      </div>
+      {/* level up  your quality  of work */}
+      <div className="flex ">
+        <div className="absolute flex items-end justify-end my-96">
+          <div className=" flex justify-end items-end">
+            <div>
+              <div className="absolute flex items-center justify-center top-2 right-5">
+                <img src={Container} alt="" className="w-1/3" />
+              </div>
+              <img
+                src={Background}
+                alt=""
+                className="w-1/2 flex items-center justify-center top-2 right-5"
+              />
+            </div>
+            <div>
+              <img src={Background1} alt="" className="w-40 h-96" />
+            </div>
+          </div>
         </div>
-        <img src={bg1} className=" w-1120px h-60 rounded-xl"></img>
-      </div>
-      <div className="flex items-center justify-center top-14">
-        {/* awepofjweaof naew f */}
-        <img src={image1} alt="" className="w-full h-13 top-14 left-16" />
-      </div>
-      <div className="flex items-center justify-center top-4">
-        {/* awepofjweaof naew f */}
-        <img src={image2} alt="" className="w-1120px h-60 " />
+        <img src={Frame840} alt="" className="w-full h-13 -my-14" />
       </div>
     </>
   );
