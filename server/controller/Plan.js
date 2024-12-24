ef32f1e951226532a0c52aa19102482ecba7188a
Harshdomadiya <harshdomadiya1014@gmail.com>
2024-12-24 15:45:40 +0530

    final2

--- server/controller/Plan.js
+++ server/controller/Plan.js
@@ -7,7 +7,7 @@ exports.getplan =async (req,res) =>{
         const{device,year,id} = req.body
         
         const amount = await prisma.softwarePlan.findFirst({where:{devices:Number(device),year:Number(year),software_id:id}})
-       // console.log(amount);
+        console.log(amount);
 
         return res.status(200).json({
             success:true,
@@ -19,7 +19,8 @@ exports.getplan =async (req,res) =>{
         //console.log(e);
         return res.status(500).json({
             success:false,
-            message:"error in amount sending"
+            message:"error in amount sending",
+            message1:e.message
         })
 
     }
