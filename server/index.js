f1588f6ed5e7a0aaa152aece417e85c396afc1a6
heppinkukadiya <heppinkukadiya@gmail.com>
2025-02-13 14:16:50 +0530

    SSL@

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const path = require("path");
-const express = require("express");
 const fs = require("fs");
 const https = require("https");
+const path = require("path");
+const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 4000;
-const SSL_PORT = 443; // Standard HTTPS port
+const HTTP_PORT = 4000;
+const HTTPS_PORT = 443;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,10 +22,10 @@ app.use(
 const signup = require("./routes/Routes");
 app.use("/api/v1", signup);
 
-// Serve the static files from the React app
+// Serve static files
 app.use(express.static(path.join(__dirname, "..", "build")));
 
-// Catch-all route to serve the React app for any unknown routes
+// Catch-all route to serve the React app
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
@@ -36,16 +36,16 @@ const options = {
     cert: fs.readFileSync("/etc/letsencrypt/live/www.actipace.com/fullchain.pem"),
 };
 
-// Create HTTPS server
-https.createServer(options, app).listen(SSL_PORT, () => {
-    console.log(`HTTPS Server running on port ${SSL_PORT}`);
+// Start HTTPS server
+https.createServer(options, app).listen(HTTPS_PORT, () => {
+    console.log(`HTTPS Server running on port ${HTTPS_PORT}`);
 });
 
-// Optionally, redirect HTTP to HTTPS
+// Optional: Redirect HTTP to HTTPS
 const httpApp = express();
 httpApp.use((req, res) => {
     res.redirect(`https://${req.headers.host}${req.url}`);
 });
-httpApp.listen(80, () => {
-    console.log("HTTP Server running on port 80 and redirecting to HTTPS");
+httpApp.listen(HTTP_PORT, () => {
+    console.log(`HTTP Server running on port ${HTTP_PORT}, redirecting to HTTPS`);
 });
