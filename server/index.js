37132055e5e432aeb3956a96a025ca7c1079e2bf
heppinkukadiya <heppinkukadiya@gmail.com>
2025-02-18 13:07:33 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(coockiePaser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://www.actipace.com",
         credentials:true,
     })
 )
