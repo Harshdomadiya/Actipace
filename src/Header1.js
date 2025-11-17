0f6f60e3ed96713dd4c219a5fc7d1be152bc9bc8
Harsh Domadiya <harshdomadiya1014@gmail.com>
2025-11-17 15:05:59 +0000

    update from VPS

--- src/Header1.js
+++ src/Header1.js
@@ -5,7 +5,7 @@ function Header1() {
   return (
     <div className="h-67.94px w-1920px  bg-[#071D2B] font-Roboto text-white text-sm flex justify-center gap-5">
       <div className="my-2">Offers protection against viruses</div>
-      <Link to="/extrapage"><div className=" my-2 underline">Download Now</div></Link>
+      <Link to="/download"><div className=" my-2 underline">Download Now</div></Link>
     </div>
   );
 }
